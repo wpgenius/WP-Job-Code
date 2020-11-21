040827015dfa37e160a0e81f81f104d88da9c681
pooja <pooja@wpgenius.in>
2020-11-21 14:14:19 +0530

    Initialized wp job plugin

---
+++ wp-job.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+Plugin Name: WP Job
+Plugin URI: https://wpgenius.in
+Description: WP Job is a simole plugin to post jobs.
+Version: 1.0
+Author: Team WPgenius
+Author URI: http://wpgenius.in/
+Text Domain: wp-job
+*/
