c932bab65777e72b69a36eb1c66c54bf3bcdac72
pooja <pooja@wpgenius.in>
2020-11-21 15:40:13 +0530

    Define plugin directory path varible Added wp jobs post type Added job type taxonomy

--- wp-job.php
+++ wp-job.php
@@ -9,3 +9,74 @@ Author: Team WPgenius
 Author URI: http://wpgenius.in/
 Text Domain: wp-job
 */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+define( 'WP_JOB_DIR_PATH', plugin_dir_path( __FILE__ ) );
+
+add_action( 'init', 'create_wp_jobs');
+function create_wp_jobs() {
+
+	$labels = array(
+		'name'                  => _x( 'WP Jobs', 'Post Type General Name', 'wp-job' ),
+		'singular_name'         => _x( 'WP Job', 'Post Type Singular Name', 'wp-job' ),
+		'menu_name'             => __( 'WP Jobs', 'wp-job' ),
+		'name_admin_bar'        => __( 'WP Jobs', 'wp-job' ),
+		'all_items'             => __( 'All WP Jobs', 'wp-job' ),
+		'add_new_item'          => __( 'Add New Job', 'wp-job' ),
+		'new_item'              => __( 'New WP Job', 'wp-job' ),
+		'edit_item'             => __( 'Edit Job', 'wp-job' ),
+		'update_item'           => __( 'Update Job', 'wp-job' ),
+		'view_item'             => __( 'View Job', 'wp-job' ),
+		'search_items'          => __( 'Search Job', 'wp-job' ),
+		'add_title'          	=> __( 'Add Job Title', 'wp-job' ),
+	
+	);
+	$args = array(
+		'labels'                => $labels,
+		'description'           => __('Holds jobs and job specific data','wp-job'),
+		'hierarchical'          => false,
+		'public'                => true,
+		'publicly_queryable' 	=> true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-portfolio',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'rewrite'            	=> array( 'slug' => 'jobs' ),
+		'capability_type'       => 'post',
+		'supports'              => array( 'title', 'editor')
+		
+	);
+	register_post_type( 'wp_jobs', $args);
+
+	$singular = 'Job Type';
+	$plural = 'Job Types';
+	$tax_labels = array(
+		'name' 			=> _x( $plural, "taxonomy general name"),
+		'singular_name' => _x( $singular, "taxonomy singular name"),
+		'search_items' 	=>  __("Search $singular"),
+		'all_items' 	=> __("All $singular"),
+		'edit_item' 	=> __("Edit $singular"),
+		'update_item' 	=> __("Update $singular"),
+		'add_new_item' 	=> __("Add New $singular"),
+		'new_item_name' => __("New $singular Name"),
+	);
+
+	register_taxonomy( strtolower($singular), 'wp_jobs', array(
+		'public' 				=> true,
+		'publicly_queryable' 	=> true,
+		'show_ui' 				=> true,
+		'show_in_nav_menus' 	=> true,
+		'hierarchical' 			=> false,
+		'query_var' 			=> true,
+		'rewrite' 				=> false,
+		'labels' 				=> $tax_labels
+	) );
+
+}
