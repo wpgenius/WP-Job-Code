38063a870f0b4a49c917612327f137c661078634
pooja <pooja@wpgenius.in>
2020-11-23 09:44:05 +0530

    Changed lables for job post

--- wp-job.php
+++ wp-job.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
-Plugin Name: WP Job
+Plugin Name: WP Job Code
 Plugin URI: https://wpgenius.in
 Description: WP Job is a simole plugin to post jobs.
 Version: 1.0
@@ -16,18 +16,26 @@ define( 'WP_JOB_DIR_PATH', plugin_dir_path( __FILE__ ) );
 
 require_once WP_JOB_DIR_PATH . 'include/plugin-actions.php';
 
+require_once WP_JOB_DIR_PATH . 'include/shortcode/job-list.php';
+
+
+function load_plugin_css() {
+		wp_enqueue_style( 'wp-job-css', plugin_dir_url( __FILE__ ) . 'assets/css/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'load_plugin_css' );
+
 
 add_action( 'init', 'create_wp_jobs');
 function create_wp_jobs() {
 
 	$labels = array(
-		'name'                  => _x( 'WP Jobs', 'Post Type General Name', 'wp-job' ),
-		'singular_name'         => _x( 'WP Job', 'Post Type Singular Name', 'wp-job' ),
-		'menu_name'             => __( 'WP Jobs', 'wp-job' ),
-		'name_admin_bar'        => __( 'WP Jobs', 'wp-job' ),
-		'all_items'             => __( 'All WP Jobs', 'wp-job' ),
+		'name'                  => _x( 'Jobs', 'Post Type General Name', 'wp-job' ),
+		'singular_name'         => _x( 'Job', 'Post Type Singular Name', 'wp-job' ),
+		'menu_name'             => __( 'Jobs', 'wp-job' ),
+		'name_admin_bar'        => __( 'Jobs', 'wp-job' ),
+		'all_items'             => __( 'All Jobs', 'wp-job' ),
 		'add_new_item'          => __( 'Add New Job', 'wp-job' ),
-		'new_item'              => __( 'New WP Job', 'wp-job' ),
+		'new_item'              => __( 'New Job', 'wp-job' ),
 		'edit_item'             => __( 'Edit Job', 'wp-job' ),
 		'update_item'           => __( 'Update Job', 'wp-job' ),
 		'view_item'             => __( 'View Job', 'wp-job' ),
@@ -71,7 +79,31 @@ function create_wp_jobs() {
 		'new_item_name' => __("New $singular Name"),
 	);
 
-	register_taxonomy( strtolower($singular), 'wp_jobs', array(
+	register_taxonomy( 'wp_job_type', 'wp_jobs', array(
+		'public' 				=> true,
+		'publicly_queryable' 	=> true,
+		'show_ui' 				=> true,
+		'show_in_nav_menus' 	=> true,
+		'hierarchical' 			=> false,
+		'query_var' 			=> true,
+		'rewrite' 				=> false,
+		'labels' 				=> $tax_labels
+	) );
+
+	$singular = 'Job Category';
+	$plural = 'Job Categories';
+	$tax_labels = array(
+		'name' 			=> _x( $plural, "taxonomy general name"),
+		'singular_name' => _x( $singular, "taxonomy singular name"),
+		'search_items' 	=>  __("Search $singular"),
+		'all_items' 	=> __("All $singular"),
+		'edit_item' 	=> __("Edit $singular"),
+		'update_item' 	=> __("Update $singular"),
+		'add_new_item' 	=> __("Add New $singular"),
+		'new_item_name' => __("New $singular Name"),
+	);
+
+	register_taxonomy( 'wp_job_cat', 'wp_jobs', array(
 		'public' 				=> true,
 		'publicly_queryable' 	=> true,
 		'show_ui' 				=> true,
