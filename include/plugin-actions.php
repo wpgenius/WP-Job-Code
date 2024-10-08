0dfa39597adfcc054a9d7c4468ef1efdfc3e5a2a
Makarand Mane <mane.makarand@gmail.com>
2024-10-08 12:27:22 +0530

    Add career page as parent item in breadcrumb

--- include/plugin-actions.php
+++ include/plugin-actions.php
@@ -17,3 +17,31 @@ function apply_templates( $template ) {
 }
 
 add_filter( 'template_include', 'apply_templates' );
+
+
+/**
+ * Filter breadcrumbs on Salary Data pages.
+ *
+ * @param array $link The link array.
+ * @param array $breadcrumb The breadcrumb item array.
+ *
+ * @return str  $link The link output.
+ */
+function yoast_seo_breadcrumb_append_link( $links ) {
+
+	if ( is_singular( 'wp_jobs' ) ) {
+
+		$page = get_page_by_path('careers');
+
+		$breadcrumb[] = array(
+            'url' => get_permalink( $page ),
+            'text' => get_the_title($page),
+        );
+
+        array_splice( $links, 1, -2, $breadcrumb );
+
+	}
+
+	return $links;
+}
+add_filter( 'wpseo_breadcrumb_links', 'yoast_seo_breadcrumb_append_link' );
